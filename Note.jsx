32608e42dd58d74bcc89a4948898038af2b7805d
Sushant2435 <76814511+Sushant2435@users.noreply.github.com>
2021-06-05 12:58:55 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js </h1>
+      <p>
+        This was an amazing bootcamp takeb up by Shaurya shinha We covered
+        everything from Scratch including Javascript and Ract.js,HTMl.
+      </p>
+    </div>
+  );
+}
+export default Header;
